3ae419daecff240bdc0cc33891b95a3b5ce44895
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-17 14:10:31 +0300

    login view add

--- src/views/login/login.tsx
+++ src/views/login/login.tsx
@@ -1,13 +1,22 @@
+import { PasswordInput } from "../../components/input/password";
+import { LoginInput } from "../../components/input/login";
 import { NavLink } from "react-router-dom";
 import styles from "./login.module.scss";
 
 export const Login = () => {
 
-    return <main>
-        <section>
-            <div>
-                <NavLink to="/login" />
-                <NavLink to="/registration" />
+    return <main className={styles.main}>
+        <section className={styles.section}>
+            <div className={styles.container}>
+                <NavLink to="/login">login</NavLink>
+                <NavLink to="/registration">registration</NavLink>
+            </div>
+            <div className={styles.password_container}>
+                <form onSubmit={(event) => event.preventDefault()} className={styles.form}>
+                    <LoginInput />
+                    <PasswordInput />
+                </form>
+                <button type="submit"></button>
             </div>
         </section>
     </main>
