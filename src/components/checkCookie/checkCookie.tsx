699383d94f4495e78e91f9d9946c3d422c758079
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-18 15:21:19 +0300

    protected router and ckec cookie update

--- src/components/checkCookie/checkCookie.tsx
+++ src/components/checkCookie/checkCookie.tsx
@@ -1,34 +1,41 @@
+import { Navigate, Outlet, useLocation } from "react-router";
 import { updateToken } from "../../redux/store/jwt";
+import { ReduxStore } from "../../interface/redux";
 import { useFetch } from "../../hooks/useFetch";
 import { Error } from "../../views/error/error";
 import { Loading } from "../loading/loading";
 import { useDispatch } from "react-redux";
-import { Navigate } from "react-router";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 export const CheckCookie = (props: { children: React.ReactNode }) => {
 
     const dispatch = useDispatch();
+    const location = useLocation();
     const {load, data, error} = useFetch('refresh');
+    const jwt = useSelector((state:ReduxStore) => state.jwt);
+
+    useEffect(() => {
+
+        if(data?.auth && data.login && data.accesstoken){
+
+            dispatch(updateToken({user: {
+
+                login: data.login,
+                jwt: data.accesstoken,
+        
+            }}));
+
+        }
+
+    },[load, data, jwt])
 
     if(load) return <Loading/>
     if(error) return <Error error={error}/>
     if(data.error) return <Error error={data.error}/>
+    if(jwt.user.jwt !== null) return <>{props.children}</>
+    if(location.pathname !== '/login' && location.pathname !== '/registration') return <Navigate to="/login" replace={true} />
 
-    if(data?.auth && data.login && data.accesstoken){
-
-        dispatch(updateToken({user: {
-
-            login: data.login,
-            jwt: data.accesstoken,
-    
-        }}));
-
-        return <>{props.children}</>
-
-    }else{
-
-        return <Navigate to="/login"/>
-
-    }
+    return <Outlet/>
 
 }
--- src/components/protectedRouter/protectedRouter.tsx
+++ src/components/protectedRouter/protectedRouter.tsx
@@ -2,7 +2,6 @@ import { Navigate, Outlet, useLocation } from "react-router";
 import { CheckCookie } from "../checkCookie/checkCookie";
 import { ReduxStore } from "../../interface/redux";
 import { useSelector } from "react-redux";
-import jwt_decode from "jwt-decode";
 import { useEffect } from "react";
 
 export const ProtectedRouter = () => {
@@ -10,24 +9,15 @@ export const ProtectedRouter = () => {
     const location = useLocation();
     const jwt = useSelector((state:ReduxStore) => state.jwt);
 
-    useEffect(() => {
-
-
-    }, [jwt])
+    useEffect(() => { console.log(jwt) }, [jwt])
 
     if(!jwt.user.jwt){
-
-        if(location.pathname === '/login' || location.pathname === '/registration') return <Outlet/>
         
         return <CheckCookie><Outlet/></CheckCookie>
 
     }else{
 
-        let decodedToken:any = jwt_decode(jwt.user.jwt);
-
-        if (decodedToken.exp * 1000 < new Date().getTime()) return <CheckCookie><Outlet/></CheckCookie>
-
-        if(location.pathname === '/login' || location.pathname === '/registration') <Navigate to="/"/>
+        if(location.pathname === '/login' || location.pathname === '/registration') return <Navigate to="/"/>
 
         return <Outlet/>
 
