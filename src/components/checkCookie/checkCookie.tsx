be939d91f7dbe9819bb4a932a7a2ce91ce77b4eb
l lThrackerzod <mentsoff.daniar@yandex.ru>
2022-07-18 08:22:07 +0400

    check cookie add

--- src/components/checkCookie/checkCookie.tsx
+++ src/components/checkCookie/checkCookie.tsx
@@ -3,18 +3,32 @@ import { useFetch } from "../../hooks/useFetch";
 import { Error } from "../../views/error/error";
 import { Loading } from "../loading/loading";
 import { useDispatch } from "react-redux";
+import { Navigate } from "react-router";
 
 export const CheckCookie = (props: { children: React.ReactNode }) => {
 
     const dispatch = useDispatch();
-    const {load, data, error} = useFetch((process.env.REACT_APP_SERVER as string) + 'refresh');
-
+    const {load, data, error} = useFetch('refresh');
+    console.log(data);
     if(load) return <Loading/>
     if(error) return <Error error={error}/>
     if(data.error) return <Error error={data.error}/>
 
-    dispatch(updateToken(data));
+    if(data?.auth && data.login && data.accesstoken){
 
-    return <>{props.children}</>
+        dispatch(updateToken({user: {
+
+            login: data.login,
+            jwt: data.accesstoken,
+    
+        }}));
+
+        return <>{props.children}</>
+
+    }else{
+
+        return <Navigate to="/login"/>
+
+    }
 
 }
