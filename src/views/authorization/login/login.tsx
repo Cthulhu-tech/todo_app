671e799f5c7b1027e9f1e61527d361cb627eed48
l lThrackerzod <mentsoff.daniar@yandex.ru>
2022-07-18 08:22:27 +0400

    aduthorization add

---
+++ src/views/authorization/login/login.tsx
@@ -0,0 +1,41 @@
+import { PasswordInput } from "../../../components/input/password";
+import { Switch } from "../../../components/switch/switch";
+import styles from "../authorization.module.scss";
+import { useForm } from "../../../hooks/useForm";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { updateToken } from "../../../redux/store/jwt";
+
+export const Login = () => {
+    
+    const dispatch = useDispatch();
+    const { data, values, onChange, handlerForm } = useForm("login");
+
+    useEffect(() => {
+
+        if(data?.auth && data.login && data.accesstoken)
+            dispatch(updateToken({user: {
+
+                login: data.login,
+                jwt: data.accesstoken,
+        
+            }}));
+
+    },[values]);
+
+    return <main className={styles.main}>
+        <section className={styles.section}>
+            <Switch/>
+            <form onSubmit={handlerForm} className={styles.form}>
+                <div className={styles.authorization_container}>
+                    <input placeholder="login" className={styles.input} type="text" onChange={onChange} value={values.login} name="login"/>
+                    <PasswordInput value={values.password} onChange={onChange} />
+                </div>
+                <button type="submit" className={styles.button}>login</button>
+            </form>
+            {data && data.message && <p className={styles.message}>{data.message}</p>}
+            {data && data.error && <p className={styles.error}>{data.error}</p>}
+        </section>
+    </main>
+
+}
---
+++ src/views/authorization/registration/registration.tsx
@@ -0,0 +1,32 @@
+import { PasswordInput } from "../../../components/input/password";
+import { Switch } from "../../../components/switch/switch";
+import styles from "../authorization.module.scss";
+import { useForm } from "../../../hooks/useForm";
+import { useEffect } from "react";
+
+export const Registration = () => {
+    
+    const { data, values, onChange, handlerForm } = useForm("regist");
+
+    useEffect(() => {
+
+        console.log(values)
+
+    },[values]);
+
+    return <main className={styles.main}>
+        <section className={styles.section}>
+            <Switch/>
+            <form onSubmit={handlerForm} className={styles.form}>
+                <div className={styles.authorization_container}>
+                    <input placeholder="login" className={styles.input} type="text" onChange={onChange} value={values.login} name="login"/>
+                    <PasswordInput value={values.password} onChange={onChange} />
+                </div>
+                <button type="submit" className={styles.button}>registration</button>
+            </form>
+            {data && data.message && <p className={styles.message}>{data.message}</p>}
+            {data && data.error && <p className={styles.error}>{data.error}</p>}
+        </section>
+    </main>
+
+}
