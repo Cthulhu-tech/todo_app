9e633401be8d28f3c59b4364a4c8971caab0a55b
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-20 22:03:17 +0300

    navigation update

--- src/layout/navigation/navigation.tsx
+++ src/layout/navigation/navigation.tsx
@@ -1,3 +1,4 @@
+import { SingIn } from '../../components/singIn/singIn';
 import { ReduxStore } from '../../interface/redux';
 import styles from './navigation.module.scss';
 import { useSelector } from 'react-redux';
@@ -5,9 +6,9 @@ import { useSelector } from 'react-redux';
 export const Navigation = () => {
 
     const user = useSelector((store: ReduxStore) => store.jwt.user.login);
-
+    
     return <nav className={styles.navigation}>
-        <p className={styles.paragraph}>{user && "user: " + user}</p>
+        {user && <SingIn/>}
     </nav>
 
 }
