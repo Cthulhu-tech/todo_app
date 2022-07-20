de08dee04d5971403f0569672452d44020d45e14
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-20 15:24:22 +0300

    update layouts

--- src/layout/navigation/navigation.tsx
+++ src/layout/navigation/navigation.tsx
@@ -1,9 +1,13 @@
+import { ReduxStore } from '../../interface/redux';
 import styles from './navigation.module.scss';
+import { useSelector } from 'react-redux';
 
 export const Navigation = () => {
 
+    const user = useSelector((store: ReduxStore) => store.jwt.user.login);
+
     return <nav className={styles.navigation}>
-        
+        <p className={styles.paragraph}>{user && "user: " + user}</p>
     </nav>
 
 }
