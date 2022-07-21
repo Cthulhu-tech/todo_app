641b91544f6c0597230c85186894aa6263d7ed45
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-21 17:57:02 +0300

    add

--- src/components/input/add/add.tsx
+++ src/components/input/add/add.tsx
@@ -1,18 +1,46 @@
+import { ReduxStore } from "../../../interface/redux";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import styles from "./add.module.scss";
 
 export const AddInput = () => {
 
-    return <div className={styles.container_input}>
-        <div className={styles["sub-input_container"]}>
-            <input className={styles.input_add} placeholder="Add new task"/>
+    const [input, setInput] = useState({text: ''});
+    const jwt = useSelector((store: ReduxStore) => store.jwt.user.jwt);
+
+    const handlerInput = (event: React.ChangeEvent<HTMLInputElement>) => setInput({...input, text: event.target.value});
+
+    const addTodo = () => {
+
+        fetch(process.env.REACT_APP_SERVER + 'api/add', {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + jwt
+            },
+            
+            body: JSON.stringify(input)
+
+        })
+
+    }
+
+    useEffect(() => {},[input]);
+
+    return  <div className={styles.container_input}>
+            <div className={styles["sub-input_container"]}>
+                <input onChange={handlerInput} className={styles.input_add} name="text" value={input.text} placeholder="Add new task"/>
+            </div>
+            <div className={styles["sub-input_container"]}>
+                <NavLink to="/" className={({isActive}) => isActive ? styles.active + ' ' + styles.link : styles.link}>All</NavLink>
+                <NavLink to="/pending" className={({isActive}) => isActive ? styles.active + ' ' + styles.link : styles.link}>Pending</NavLink>
+                <NavLink to="/completed" className={({isActive}) => isActive ? styles.active + ' ' + styles.link : styles.link}>Completed</NavLink>
+                <button className={styles.btn_add} onClick={addTodo}>Add</button>
+            </div>
         </div>
-        <div className={styles["sub-input_container"]}>
-            <NavLink to="/" className={({isActive}) => isActive ? styles.active + ' ' + styles.link : styles.link}>All</NavLink>
-            <NavLink to="/pending" className={({isActive}) => isActive ? styles.active + ' ' + styles.link : styles.link}>Pending</NavLink>
-            <NavLink to="/completed" className={({isActive}) => isActive ? styles.active + ' ' + styles.link : styles.link}>Completed</NavLink>
-            <button className={styles.btn_add}>Add</button>
-        </div>
-    </div>
 
 }
