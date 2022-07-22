9b62133e94b455d9934d75197f5bef34e171c04c
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-07-22 16:47:52 +0300

    heroku env add

--- src/components/todo/handler/handler.tsx
+++ src/components/todo/handler/handler.tsx
@@ -1,23 +1,29 @@
-import { dateFormeter } from "../../../utils/dataformater";
 import {completePending, deleteCompleted, uncompleteCompleted} from "../../../redux/store/todo";
-import {deletePending} from "../../../redux/store/todo";
 import { ReduxStore, TodoType } from "../../../interface/redux";
+import { dateFormeter } from "../../../utils/dataformater";
+import { FetchDataTodo } from "../../../utils/fetchData";
+import {deletePending} from "../../../redux/store/todo";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import styles from "../todo.module.scss";
-import { FetchDataTodo } from "../../../utils/fetchData";
+import { useState } from "react";
 
 export const HandlerTodo = (data: TodoType) => {
 
     const dispatch = useDispatch();
+    const [load, setLoad] = useState(false);
     const jwt = useSelector((store: ReduxStore) => store.jwt.user.jwt);
 
     const deleteTodo = (_id:number, _completed: number) => {
+
+        setLoad(true);
 
         if(_completed === 0){
 
             FetchDataTodo("api/delete", _id, jwt)
                 .then(() => {
+
+                    setLoad(false);
 
                     dispatch(deletePending(_id));
 
@@ -27,6 +33,8 @@ export const HandlerTodo = (data: TodoType) => {
 
             FetchDataTodo("api/delete", _id, jwt)
             .then(() => {
+
+                setLoad(false);
 
                 dispatch(deleteCompleted(_id));
 
@@ -38,19 +46,36 @@ export const HandlerTodo = (data: TodoType) => {
 
     const completeTodo = (_id:number, _completed: number) => {
 
+        setLoad(true);
+
         if(_completed === 0){
 
-            dispatch(completePending(_id));
+            FetchDataTodo("api/update", _id, jwt, "PUT")
+            .then(() => {
+
+                setLoad(false);
+
+                dispatch(completePending(_id));
+
+            });
 
         }else{
 
-            dispatch(uncompleteCompleted(_id));
+            FetchDataTodo("api/update", _id, jwt, "PUT")
+            .then(() => {
+
+                setLoad(false);
+                
+                dispatch(uncompleteCompleted(_id));
+
+            });
 
         }
 
     }
 
     return <div className={styles.todo_container}>
+            {load && <div className={styles.load}/>}
             <div className={styles.button} onClick={() => completeTodo(data.id, data.completed)}>{data.completed === 0 ? "" : "✓"}</div>
             <p className={styles.paragraph_time}>{dateFormeter(data.todo_time_start)}</p>
             <p className={styles.paragraph_text}>{data.todo_text}</p>
--- src/utils/fetchData.tsx
+++ src/utils/fetchData.tsx
@@ -1,15 +1,21 @@
-export const FetchDataTodo = (url: string, data: number, jwt: string | null) => {
+export const FetchDataTodo = (url: string, data: number, jwt: string | null, method = "DELETE") => {
 
     return fetch(process.env.REACT_APP_SERVER + url,
                 {
-                    method: 'DELETE',
+                    mode: 'cors',
+                    redirect: 'follow',
+                    credentials: "include",
+                    method,
+
                     headers: {
 
-                        'Authorization': `Bearer ${jwt}`
+                        'Authorization': `Bearer ${jwt}`,
+                        'Content-type': 'application/json',
+                        'Accept': 'application/json'
 
                     },
 
-                    body: JSON.stringify(data)
+                    body: JSON.stringify({id: data})
 
                 })
                 .then((response) => {
